f1803909490e496fe84ce2db267c44b027dc79e8
d11416146 <d11416146@gs.takming.edu.tw>
2026-04-24 14:02:18 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Data;
+
+namespace midtest_01_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int a = 15;
+                int b = 5;
+                int c = a / b;
+                string[] names = new string[5];
+                string id = names[4];
+
+                if (b > 10)
+                {
+                    throw new ArithmeticException("b大於10");
+                }
+
+            }
+            catch(DivideByZeroException ex)
+            {
+                Console.WriteLine("Error1");
+                Console.WriteLine(ex.ToString());
+            }
+            catch(IndexOutOfRangeException ex)
+            {
+                Console.WriteLine("Error2");
+                Console.WriteLine(ex.ToString());
+            }
+            catch(ArithmeticException ex)
+            {
+                Console.WriteLine("Error3");
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                Console.WriteLine("最終執行");
+            }
+        }
+    }
+}
